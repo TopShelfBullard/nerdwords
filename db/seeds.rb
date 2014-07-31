f4e79245ad8774004a0f1e6323c57c67fa9fd40a
TopShelfBullard <amb@amitchellbullard.com>
2014-07-31 09:48:13 -0500

    Clean up seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,32 +31,65 @@ Length.find_each do |n|
 end
 
 File.open("db/word_list.txt", "r") do |file|
-    file.each_line do |line|
-        if line.chomp[/[a-zA-Z]+/] == line.chomp
-            current_word = line.chomp.downcase
-            current_word_length = current_word.length
+  file.each_line do |line|                                          
+    if line.chomp[/[a-zA-Z]+/] == line.chomp
+      current_word = line.chomp.downcase
+      current_word_length = current_word.length
 
-            included_letters = current_word.split('')
-            word_first_letter_record = FirstLetter.find_by letter: included_letters.first
-            word_last_letter_record = LastLetter.find_by letter: included_letters.last
+      included_letters = current_word.split('')
+      word_first_letter_record = FirstLetter.find_by letter: included_letters.first
+      word_last_letter_record = LastLetter.find_by letter: included_letters.last
 
-            sorted_letters = included_letters.sort
-            sorted_first_letter_record = FirstLetter.find_by letter: sorted_letters.first
-            sorted_last_letter_record = LastLetter.find_by letter: sorted_letters.last
+      sorted_letters = included_letters.sort
+      sorted_first_letter_record = FirstLetter.find_by letter: sorted_letters.first
+      sorted_last_letter_record = LastLetter.find_by letter: sorted_letters.last
 
-            joined_sort = sorted_letters.join
+      joined_sort = sorted_letters.join
 
-            if Sort.find_by(inclusion: joined_sort, first_letter_id: sorted_first_letter_record.id, last_letter_id: sorted_last_letter_record.id, length_id: current_word_length)
-                current_sort = Sort.find_by(inclusion: joined_sort, first_letter_id: sorted_first_letter_record.id, last_letter_id: sorted_last_letter_record.id, length_id: current_word_length)
-            else
-                current_sort = Sort.create(inclusion: joined_sort, first_letter_id: sorted_first_letter_record.id, last_letter_id: sorted_last_letter_record.id, length_id: current_word_length)
-            end
+      if Sort.find_by(inclusion: joined_sort,
+                      first_letter_id: sorted_first_letter_record.id,
+                      last_letter_id: sorted_last_letter_record.id,
+                      length_id: current_word_length)
+        current_sort = Sort.find_by(inclusion: joined_sort,
+                                    first_letter_id: sorted_first_letter_record.id,
+                                    last_letter_id: sorted_last_letter_record.id,
+                                    length_id: current_word_length)
+      else
+        current_sort = Sort.create(inclusion: joined_sort,
+                                   first_letter_id: sorted_first_letter_record.id,
+                                   last_letter_id: sorted_last_letter_record.id,
+                                   length_id: current_word_length)
+      end
 
-            new_word = Word.create(text: current_word, sort_id: current_sort.id, first_letter_id: word_first_letter_record.id, last_letter_id: word_last_letter_record.id, length_id: current_word_length)
-            puts "Word -- id:#{new_word.id}  text:#{new_word.text}  sort_id:#{new_word.sort_id}  first_letter_id:#{new_word.first_letter_id}  last_letter_id:#{new_word.last_letter_id}  length_id:#{new_word.length_id}"
-            puts "FirstLetter -- #{FirstLetter.find(new_word.first_letter_id)} -- LastLetter -- #{LastLetter.find(new_word.last_letter_id)} -- Length -- #{Length.find(new_word.length_id)}"
-            puts "Sort -- id:#{current_sort.id}  inclusion:#{current_sort.inclusion}  first_letter_id:#{current_sort.first_letter_id}  last_letter_id:#{current_sort.last_letter_id}  length_id:#{current_sort.length_id}"
-            puts "FirstLetter -- #{FirstLetter.find(current_sort.first_letter_id)} -- LastLetter -- #{LastLetter.find(current_sort.last_letter_id)} -- Length -- #{Length.find(current_sort.length_id)}"
-        end
-    end
+      new_word = Word.create(text: current_word,
+                             sort_id: current_sort.id,
+                             first_letter_id: word_first_letter_record.id,
+                             last_letter_id: word_last_letter_record.id,
+                             length_id: current_word_length)
+    
+      id = new_word.id
+      text = new_word.text
+      sort = current_sort.inclusion
+      first_letter_record = FirstLetter.find(new_word.first_letter_id)
+      first_letter = first_letter_record.letter
+      last_letter_record = LastLetter.find(new_word.last_letter_id)
+      last_letter = last_letter_record.letter
+      length_record = Length.find(new_word.length_id)
+      length = length_record.number
+
+      puts "id:#{id} text:#{text} sort:#{sort} f_letter:#{first_letter} l_letter:#{last_letter} lenght:#{length}"
+
+      id = current_sort.id
+      first_letter_record = FirstLetter.find(current_sort.first_letter_id)
+      first_letter = first_letter_record.letter
+      last_letter_record = LastLetter.find(current_sort.last_letter_id)
+      last_letter = last_letter_record.letter
+      length_record = Length.find(current_sort.length_id)
+      length = length_record.number
+
+      puts "id:#{id} sort:#{sort} f_letter:#{first_letter} l_letter:#{last_letter} lenght:#{length}"
+
+      puts ""
+    end  
+  end
 end
